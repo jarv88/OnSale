21091cb304461e94a1f1e8347dee34a0f97e477d
jarv88 <joseangel038@gmail.com>
2021-08-28 00:23:09 -0500

    Initial configuration, create class Country as Model and as Controller, create DataContext, and make first migration

---
+++ OnSale.Web/Controllers/CountriesController.cs
@@ -0,0 +1,153 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnSale.Common.Entities;
+using OnSale.Web.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnSale.Web.Controllers
+{
+    public class CountriesController : Controller
+    {
+        private readonly DataContext _context;
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+        // GET: CountriesController
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Countries.ToListAsync());
+        }
+
+        // GET: CountriesController/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var country = await _context.Countries
+                        .FirstOrDefaultAsync(m => m.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return View(country);
+        }
+
+        // GET: CountriesController/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CountriesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Country country) // [Bind("Id,Name")]
+        {
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(country);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(country);
+        }
+
+        // GET: CountriesController/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var country = await _context.Countries.FindAsync(id);
+
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return View(country);
+        }
+
+        // POST: CountriesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Country country)
+        {
+            if (id != country.Id)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(country);
+                    await _context.SaveChangesAsync();
+
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    //if (!CountryExists(country.Id))
+                    //{
+                    //    return NotFound();
+                    //}
+                    //else
+                    //{
+                    //    throw;
+                    //}
+
+                    
+                }
+                return RedirectToAction("Index");
+
+            }
+  
+            return View(country);
+        }
+
+        // GET: CountriesController/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var country = await _context.Countries
+                        .FirstOrDefaultAsync(m => m.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return View(country);
+        }
+
+        // POST: CountriesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteCountry(int? id)
+        {
+            var country = _context.Countries.Find(id);
+
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            _context.Countries.Remove(country);
+            await _context.SaveChangesAsync();
+            //TempData["message"] = "The category has deleted successfully";
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ OnSale.Web/Data/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnSale.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnSale.Web.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+               
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        //Para indicar que no se peuden guardar 2 paises con el mismo nombre
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>()
+                .HasIndex(t => t.Name)
+                .IsUnique();
+        }
+
+    }
+}
